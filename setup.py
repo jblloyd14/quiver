fc067b6d3e49ac863c0ca830d325c4835c9988e9
jblloyd14 <lloyd.brad@gmail.com>
2025-03-08 11:43:25 +0000

    upped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="quiver",
-    version="0.0.87",
+    version="0.0.88",
     author="some guy",
     author_email="@finblocks.com",
     description="a library for managing and querying time series data",
