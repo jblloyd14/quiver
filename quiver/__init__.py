7cbae4007e39a3a71c49dbfdb4b707029e0f76f7
jblloyd14 <lloyd.brad@gmail.com>
2025-05-26 21:00:43 +0100

    upped version

--- quiver/__init__.py
+++ quiver/__init__.py
@@ -1,11 +1,11 @@
 from .library import Library
-from .utils import (read_csv, set_path, get_path,
+from .utils import (set_path, get_path,
     get_lib_size, get_subject_size, get_item_size,
     write_metadata, read_metadata,
     set_partition_size, get_partition_size,
     list_libraries, delete_libraries, delete_library,)
 
-__version__ = "0.1.01"
+__version__ = "0.1.02"
 __author__ = "Some Guy"
 
 __all__ = ["Library", "get_path", "set_path",
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="quiver",
-    version="0.1.01",
+    version="0.1.02",
     author="some guy",
     author_email="@finblocks.com",
     description="a library for managing and querying time series data",
