18cf02af31d9979e32553c14cdf21a31cddaf641
jblloyd14 <lloyd.brad@gmail.com>
2025-05-29 12:31:23 +0100

    added schema funcs

--- quiver/__init__.py
+++ quiver/__init__.py
@@ -3,7 +3,8 @@ from .utils import (set_path, get_path,
     get_lib_size, get_subject_size, get_item_size,
     write_metadata, read_metadata,
     set_partition_size, get_partition_size,
-    list_libraries, delete_libraries, delete_library,)
+    list_libraries, delete_libraries, delete_library,
+    suggest_subject_schema, write_subject_schema,)
 
 __version__ = "0.1.03"
 __author__ = "Some Guy"
@@ -12,4 +13,5 @@ __all__ = ["Library", "get_path", "set_path",
            "get_lib_size", "get_subject_size", "get_item_size",
            "write_metadata", "read_metadata",
            "set_partition_size", "get_partition_size",
-           "list_libraries", "delete_libraries", "delete_library"]
+           "list_libraries", "delete_libraries", "delete_library",
+           "suggest_subject_schema", "write_subject_schema"]
